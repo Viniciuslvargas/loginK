2799f9e4f2de82cf618ebc59b9af0959499ec611
Viniciuslvargas <vinicius_l_vargas@estudante.sesisenai.org.br>
2024-09-19 12:25:06 -0300

    gg

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,23 @@
+describe('pagina de login', () => {
+  it('login com sucesso', () => {
+    cy.visit('http://127.0.0.1:5500//index.html');
+    cy.get('#usuario').type('admin');
+    cy.get('#senha').type('admin');
+    cy.get('#entrar').click()
+
+    cy.on('window.alert', (txt) => {
+      expect(txt).to.contains('login efetuado!')
+    });
+
+  });
+
+  it('Login com erro', () => {
+    cy.visit('http://127.0.0.1:5500/index.html');
+
+    cy.get('#usuario').type('1234');
+    cy.get('#senha').type('errado');
+    cy.get('#entrar').click()
+
+    cy.get('#erro').should('be.visible')
+  });
+})
